42568dd28dbbd48864b9d97d193b569c04d191d7
sanju123-s <1ms24mc091@msrit.edu>
2025-11-13 12:52:21 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,8 @@
+public class first {
+    public static void main(String[] args) {
+        System.out.print("Hello, ");
+        System.out.print("World!");
+        System.out.print(" Without a new line.");
+        System.out.println(); // To add a new line at the end
+    }
+}
